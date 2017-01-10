3274c2b199342f8ea6ec8e0db18bc759798aed45
alexsingal <alexander.singal@gmail.com>
2017-01-10 11:30:23 -0500

    updates

--- knn_stocks_example.R
+++
@@ -1,9 +0,0 @@
-require(class)
-
-lag1_lag2 <- cbind(Smarket$Lag1, Smarket$Lag2)
-train <- Smarket$Year < 2005
-test <- Smarket$Year == 2005
-knn.pred <- knn(lag1_lag2[train, ], lag1_lag2[test, ], Smarket$Direction[train], k=1)
-table(knn.pred, Smarket$Direction[test])
-mean(knn.pred == Smarket$Direction[test])
- 
